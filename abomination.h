3bacd7c6e5d412a749e1d579d0b60aaaaf11948e
Christian Gunderman <christian@sonofboss.cwru.edu>
2014-05-05 11:36:15 -0400

    Added abomination.h and example.c
    Signed-off-by: Christian Gunderman <christian@sonofboss.cwru.edu>

---
+++ abomination.h
@@ -0,0 +1,31 @@
+/**
+ * abomination.h
+ * (C) 2014 Christian Gunderman
+ * Modified by:
+ * Author Email: gundermanc@gmail.com
+ * Modifier Email:
+ *
+ * Description:
+ * Visual basic like macros for C. Created this project because I was bored.
+ * If you ACTUALLY use this, you should probably cover your mouth with one hand
+ * and your nose with another and hold it that way until things go dark ;)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ABOMINATION__H__
+#define ABOMINATION__H__
+
+
+#endif  /* ABOMINATION__H__ */
